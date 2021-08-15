a1c437d18284b27446a7c32e3966bc5bec7985bd
Avichay Ben Lulu <40361913+avichbe@users.noreply.github.com>
2021-08-15 17:03:46 +0300

    Update Singelton.py

--- Design-Patterns/Singelton.py
+++ Design-Patterns/Singelton.py
@@ -1,5 +1,5 @@
 """
-in this file i'm impelented a simple singelton.
+in this file i'm implemented a simple singelton.
 from it's name we can understand that this pattern allow only one instance of the class.
 most common use for this pattern is in connection to DB, or when you want to save data after initialized new screen
 (like in android)
